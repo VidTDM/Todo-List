7df6226331da1f5ba750c2f8774d6be8bcf52293
VidTDM <realvidtdm@gmail.com>
2022-03-26 09:22:46 +0530

    Added Animations

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ switch (localStorage.getItem('todos')) {
         localStorage.setItem('todos', JSON.stringify([]));
         break;
     default:
-        setTodos(JSON.parse(localStorage.getItem('todos')));
+        setTodos(JSON.parse(localStorage.getItem('todos')), false);
         break;
 }
 
-function setTodos(todos) {
+function setTodos(todos, create) {
     const ul = document.querySelector('ul.todo-list');
     ul.innerHTML = '';
-    todos.forEach(el => {
+    todos.forEach((el, i) => {
         const todo = document.createElement('div');
         const li = document.createElement('li');
         const span = document.createElement('span');
@@ -21,12 +21,14 @@ function setTodos(todos) {
         span.textContent = el.tag;
         li.appendChild(span);
         btn.innerHTML = '<i class="fas fa-trash"></i>';
-        btn.setAttribute('onclick', 'deleteTodo(this.parentNode.id)');
+        btn.setAttribute('onclick', 'deleteTodo(this.parentNode)');
         btn.setAttribute('class', 'btn btn-danger');
         todo.appendChild(li);
         todo.appendChild(btn);
         todo.setAttribute('id', el.id);
-        todo.setAttribute('class', 'todo');
+        todo.setAttribute('class', 'todo animate__animated');
+        if (i === 0 && create === true)
+            todo.classList.add('animate__lightSpeedInLeft');
 
         ul.appendChild(todo);
     });
@@ -59,13 +61,30 @@ function createTodo() {
     ));
     todoTextInput.value = '';
     tagTextInput.value = '';
-    setTodos(JSON.parse(localStorage.getItem('todos')));
+    setTodos(JSON.parse(localStorage.getItem('todos')), true);
     closeButton.click();
 }
 
-function deleteTodo(id) {
+function deleteTodo(elem) {
     const todos = JSON.parse(localStorage.getItem('todos'));
-    const deletedTodos = todos.filter(el => el.id != id);
-    localStorage.setItem('todos', JSON.stringify(deletedTodos));
-    setTodos(JSON.parse(localStorage.getItem('todos')));
+    const deletedTodos = todos.filter(el => el.id != elem.id);
+    elem.classList.add('animate__lightSpeedOutRight')
+    setTimeout(() => {
+        localStorage.setItem('todos', JSON.stringify(deletedTodos));
+        setTodos(JSON.parse(localStorage.getItem('todos')), false);
+    }, 1000);
+}
+
+document.querySelectorAll('input.form-control')[1].onkeydown = e => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        document.querySelector('.create').focus();
+    }
+}
+
+document.querySelectorAll('input.form-control')[0].onkeydown = e => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        document.querySelectorAll('input.form-control')[1].focus();
+    }
 }
